034f723254291a2e24740de5dd783499a06b1e3a
加藤 倖都 <info@imadoki.co.jp>
2026-06-19 21:19:11 +0900

    Update daily_generator.py

--- daily_generator.py
+++ daily_generator.py
@@ -408,8 +408,8 @@ def run():
         ok = generate_chart_image(post["chart"], out_path)
         if ok:
             if GITHUB_REPOSITORY:
-                url = (f"https://raw.githubusercontent.com/"
-                       f"{GITHUB_REPOSITORY}/main/post-images/{filename}")
+                owner, repo = GITHUB_REPOSITORY.split("/", 1)
+url = (f"https://{owner}.github.io/{repo}/post-images/{filename}")
             else:
                 url = str(out_path)
             image_urls[i] = url
